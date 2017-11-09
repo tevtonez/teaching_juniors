e0c0587822eb9cfabe8e7e2f7a3e4955800f1393
tevtonez <tevtonez@gmail.com>
2017-11-09 13:53:10 +0100

    finish part 3

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, render, redirect
+
 from django.http import HttpResponse
+from django.http import Http404
+
+from django.template import loader
 
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -25,5 +28,21 @@ class pollsIndexView(TemplateView):
 
 
 def index(request):
-    return HttpResponse("hi there")
+    latest_question_list = models.Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
 
+    return render(request,'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(models.Question,pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+    response = "This is question's %s results"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("This is view for voting for a question %s" % question_id)
